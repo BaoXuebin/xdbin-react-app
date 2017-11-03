87768f220820b754d196003770c17741b8104dfe
BaoXuebin <baoxbin@hotmail.com>
2017-11-03 23:16:27 +0800

    修改播放器控制栏的屏幕自适应

--- src/app/lab/player/containers/Control.jsx
+++ src/app/lab/player/containers/Control.jsx
@@ -37,54 +37,145 @@ class Control extends Component {
             <div id="playerControl" className="Control">
                 <Container>
                     <Grid>
-                        <Grid.Column width={2}>
-                            <Button.Group>
-                                <Button icon="step backward" color="red" />
-                                <Button icon="pause" color="red" />
-                                <Button icon="step forward" color="red" />
-                            </Button.Group>
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <ProgressBar percent={playProgress} onChangeProgress={this.handleChangePlayProgress} />
-                        </Grid.Column>
-                        <Grid.Column width={2} style={{ marginLeft: '-1.5rem' }}>
-                            12:00/12:00
-                        </Grid.Column>
-                        <Grid.Column width={2} style={{ marginLeft: '-3rem' }}>
-                            <div style={{ float: 'left' }}>
-                                <Icon name="volume down" size="large" />
-                            </div>
-                            <ProgressBar
-                                offsetX="20%"
-                                percent={volumeProgress}
-                                onChangeProgress={this.handleChangeVolumeProgress}
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={2} style={{ marginLeft: '3rem' }}>
-                            <Button.Group floated="right" basic>
-                                <Popup
-                                    trigger={<Button icon="random" />}
-                                    on="hover"
-                                    content="随机"
-                                    position="top center"
-                                    size="mini"
+                        <Grid.Row only="computer">
+                            <Grid.Column width={2}>
+                                <Button.Group>
+                                    <Button icon="step backward" color="red" />
+                                    <Button icon="pause" color="red" />
+                                    <Button icon="step forward" color="red" />
+                                </Button.Group>
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <ProgressBar percent={playProgress} onChangeProgress={this.handleChangePlayProgress} />
+                            </Grid.Column>
+                            <Grid.Column width={2} style={{ marginLeft: '-1.5rem' }}>
+                                12:00/12:00
+                            </Grid.Column>
+                            <Grid.Column width={2} style={{ marginLeft: '-3rem' }}>
+                                <div style={{ float: 'left' }}>
+                                    <Icon name="volume down" size="large" />
+                                </div>
+                                <ProgressBar
+                                    offsetX="20%"
+                                    percent={volumeProgress}
+                                    onChangeProgress={this.handleChangeVolumeProgress}
                                 />
-                                <Popup
-                                    trigger={<Button content="词" floated="right" />}
-                                    on="hover"
-                                    content="歌词"
-                                    position="top center"
-                                    size="mini"
+                            </Grid.Column>
+                            <Grid.Column width={2} style={{ marginLeft: '3rem' }}>
+                                <Button.Group floated="right" basic>
+                                    <Popup
+                                        trigger={<Button icon="random" />}
+                                        on="hover"
+                                        content="随机"
+                                        position="top center"
+                                        size="mini"
+                                    />
+                                    <Popup
+                                        trigger={<Button content="词" floated="right" />}
+                                        on="hover"
+                                        content="歌词"
+                                        position="top center"
+                                        size="mini"
+                                    />
+                                    <Popup
+                                        trigger={<Button icon="list" floated="left" onClick={this.handleToggleMusicList} />}
+                                        on="hover"
+                                        content="播放列表"
+                                        position="top center"
+                                        size="mini"
+                                    />
+                                </Button.Group>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row only="tablet">
+                            <Grid.Column width={2}>
+                                <Button.Group>
+                                    <Button icon="step backward" color="red" />
+                                    <Button icon="pause" color="red" />
+                                    <Button icon="step forward" color="red" />
+                                </Button.Group>
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <ProgressBar percent={playProgress} onChangeProgress={this.handleChangePlayProgress} />
+                            </Grid.Column>
+                            <Grid.Column width={2} style={{ marginLeft: '-1.5rem' }}>
+                                12:00/12:00
+                            </Grid.Column>
+                            <Grid.Column width={2} style={{ marginLeft: '-3rem' }}>
+                                <div style={{ float: 'left' }}>
+                                    <Icon name="volume down" size="large" />
+                                </div>
+                                <ProgressBar
+                                    offsetX="20%"
+                                    percent={volumeProgress}
+                                    onChangeProgress={this.handleChangeVolumeProgress}
                                 />
-                                <Popup
-                                    trigger={<Button icon="list" floated="left" onClick={this.handleToggleMusicList} />}
-                                    on="hover"
-                                    content="播放列表"
-                                    position="top center"
-                                    size="mini"
-                                />
-                            </Button.Group>
-                        </Grid.Column>
+                            </Grid.Column>
+                            <Grid.Column width={2} style={{ marginLeft: '3rem' }}>
+                                <Button.Group floated="right" basic>
+                                    <Popup
+                                        trigger={<Button icon="random" />}
+                                        on="hover"
+                                        content="随机"
+                                        position="top center"
+                                        size="mini"
+                                    />
+                                    <Popup
+                                        trigger={<Button content="词" floated="right" />}
+                                        on="hover"
+                                        content="歌词"
+                                        position="top center"
+                                        size="mini"
+                                    />
+                                    <Popup
+                                        trigger={<Button icon="list" floated="left" onClick={this.handleToggleMusicList} />}
+                                        on="hover"
+                                        content="播放列表"
+                                        position="top center"
+                                        size="mini"
+                                    />
+                                </Button.Group>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row only="mobile" style={{ margin: '-.2rem 0 -1.8rem 0' }}>
+                            <Grid.Column width={12}>
+                                <ProgressBar percent={playProgress} onChangeProgress={this.handleChangePlayProgress} />
+                            </Grid.Column>
+                            <Grid.Column width={4} style={{ marginLeft: '-1.5rem' }}>
+                                12:00/12:00
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row columns={3} only="mobile">
+                            <Grid.Column width={3}>
+                                <Button.Group floated="left" basic>
+                                    <Popup
+                                        trigger={<Button icon="random" />}
+                                        on="hover"
+                                        content="随机"
+                                        position="top center"
+                                        size="mini"
+                                    />
+                                </Button.Group>
+                            </Grid.Column>
+                            <Grid.Column width={10} textAlign="center">
+                                <Button.Group>
+                                    <Button icon="step backward" color="red" />
+                                    <Button icon="pause" color="red" />
+                                    <Button icon="step forward" color="red" />
+                                </Button.Group>
+                            </Grid.Column>
+                            <Grid.Column width={3} textAlign="right">
+                                <Button.Group basic>
+                                    <Popup
+                                        trigger={<Button icon="list" floated="left" onClick={this.handleToggleMusicList} />}
+                                        on="hover"
+                                        content="播放列表"
+                                        position="top center"
+                                        size="mini"
+                                    />
+                                </Button.Group>
+                            </Grid.Column>
+                        </Grid.Row>
                     </Grid>
                 </Container>
             </div>
